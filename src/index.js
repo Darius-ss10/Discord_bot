d7386344dada4dc8f5bf36bdb1f615cbc7d459da
Darius-ss10 <darius.muntean@student.uclouvain.be>
2023-09-08 18:17:59 +0200

    create a slash ( / ) command

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import { config } from 'dotenv';
-import { Client, GatewayIntentBits } from 'discord.js';
+import { Client, GatewayIntentBits, Routes } from 'discord.js';
+import { REST } from '@discordjs/rest';
 
 config();
+
+const TOKEN = process.env.DISCORD_BOT_TOKEN;
+const CLIENT_ID = process.env.DISCORD_CLIENT_ID;
+const GUILD_ID = process.env.DISCORD_GUILD_ID;
 
 const client = new Client({ 
     intents: [
@@ -10,16 +15,38 @@ const client = new Client({
         GatewayIntentBits.MessageContent] 
     });
 
-const TOKEN = process.env.DISCORD_BOT_TOKEN;
-
-client.login(TOKEN);
+const rest = new REST({ version: '10' }).setToken(TOKEN);
 
 client.on('ready', () => {
   console.info(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('messageCreate', (message) => {
-  if (message.content === 'ping') {
-    message.reply('pong');
-  }
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    if (interaction.commandName === 'ping') {
+        await interaction.reply('Pong!');
+    }
 });
+
+async function main() {
+    const commands = [{
+        name: 'ping',
+        description: 'Replies with Pong!'
+    }];
+
+
+    try {
+        console.log('Started refreshing application (/) commands.');
+        await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { 
+            body: commands
+        });
+
+        client.login(TOKEN);
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+main();
