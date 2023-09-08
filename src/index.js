a09446984917ae0743b8278f51d74b1b6f803612
Darius-ss10 <darius.muntean@student.uclouvain.be>
2023-09-08 17:41:58 +0200

    listen and handle events

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import { config } from 'dotenv';
-import { Client } from 'discord.js';
+import { Client, GatewayIntentBits } from 'discord.js';
 
 config();
 
-const client = new Client({ intents: ['Guilds', 'GuildMessages'] });
+const client = new Client({ 
+    intents: [
+        GatewayIntentBits.Guilds, 
+        GatewayIntentBits.GuildMessages, 
+        GatewayIntentBits.MessageContent] 
+    });
 
 const TOKEN = process.env.DISCORD_BOT_TOKEN;
 
 client.login(TOKEN);
+
+client.on('ready', () => {
+  console.info(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', (message) => {
+  if (message.content === 'ping') {
+    message.reply('pong');
+  }
+});
